27426b897dfd0d2c8ca7df2f8726b05ace128f37
Lannion <jared8921@gmail.com>
2025-04-20 08:36:03 +0800

    final

---
+++ static/js/script.js
@@ -0,0 +1,8 @@
+// ============== TYPED JS ================
+const typed = new Typed(".multiple-text", {
+    strings: ["Back End Developer", "Software Engineer", "Graphic Designer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true,
+  });
